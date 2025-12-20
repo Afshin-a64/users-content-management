0c1f0106b4b2fddc2bfb8182b9f7f6a93e8d2250
Afshin-a64 <a.ataei.n11@gmail.com>
2025-12-20 23:17:59 +0330

    create main divs

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-
 import './index.css'
 
 function App() {
 
   return (
     <div>
-      
+      <div className='fixed top-0 left-0 bg-green-200 w-full h-screen pt-16 pr-64'>content</div>
+      <div className='fixed top-0 left-0 bg-amber-200 w-full h-16'>Header</div>
+      <div className='fixed top-16 right-0 bg-blue-200 w-64 h-screen'>sidebar</div>
     </div>
   )
 }
